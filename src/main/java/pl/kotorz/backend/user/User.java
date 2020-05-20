c464e7f58b43b07f5e21f1a80fa114aa87200dd0
Krystian Komor <krystian.komor@gmail.com>
2020-05-20 20:56:49 +0200

    Add user entity and repository

---
+++ src/main/java/pl/kotorz/backend/user/User.java
@@ -0,0 +1,57 @@
+package pl.kotorz.backend.user;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "user_id", nullable = false)
+	Long id;
+
+	@Column(nullable = false)
+	String email;
+
+	@Column(nullable = false)
+	String name;
+
+	@Column(nullable = false)
+	String password;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
