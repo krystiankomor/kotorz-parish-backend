ba968251e0d9071e3dc9aa3d63a9c741eba4f36f
Krystian Komor <krystian.komor@gmail.com>
2020-05-20 21:06:09 +0200

    Add REST repository to post entity

---
+++ src/main/java/pl/kotorz/backend/post/PostRestController.java
@@ -0,0 +1,27 @@
+package pl.kotorz.backend.post;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/v1/posts")
+public class PostRestController {
+	@Autowired
+	PostRepository postRepository;
+
+	@GetMapping(value = "/")
+	public Iterable<Post> getAllPosts() {
+		return postRepository.findAll();
+	}
+	
+	@GetMapping(value = "/{id}")
+	public Optional<Post> findById(@PathVariable Long id) {
+		return postRepository.findById(id);
+	}
+
+}
