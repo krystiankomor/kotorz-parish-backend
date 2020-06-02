2a56c89d1da749c7e981a18e874c3ff742454eaf
Krystian Komor <krystian.komor@gmail.com>
2020-06-02 20:58:27 +0200

    Add tests display name

--- src/test/java/pl/kotorz/backend/PostTests.java
+++ src/test/java/pl/kotorz/backend/PostTests.java
@@ -1,14 +1,18 @@
 package pl.kotorz.backend;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import pl.kotorz.backend.post.Post;
 
 @SpringBootTest
+@DisplayName("The post entity test")
 public class PostTests {
 
 	@Test
+	@DisplayName("Testing the slug generation")
 	void postSlugTest() {
 		Post post = new Post();
 		post.setTitle("Zażółć gęślą jaźń.");
